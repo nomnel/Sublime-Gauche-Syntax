73e9f2cdc295eea98e3b9475780221e2f8221efd
nomnel <laxyz.nomnel@gmail.com>
2012-10-12 14:26:21 +0900

    add R5RS highlight list file.

---
+++ r5rs-highlight-list.scm
@@ -0,0 +1,28 @@
+; line comment
+
+; #f, #t
+(#f #t)
+; number
+(1 #e1 #i1 #b1 #o1 #d1 #x1)
+; string
+("string")
+
+; define with name
+(define name ) (define-syntax name )
+; define with name, args
+(define (name arg1 arg2) )
+; lambda with args
+(lambda (arg1 arg2) )
+; named let
+(let loop )
+; define syntax
+(define-syntax ) (let-syntax ) (letrec-syntax ) (syntax-rules )
+; lambda
+(define ) (lambda ) (let ) (let* ) (letrec ) (do )
+; control
+(if ) (cond ) (else ) (case ) (and ) (or ) (begin ) (dynamic-wind )
+; misc keywords
+(force ) (delay ) (map ) (for-each ) (eval )
+(call/cc ) (call-with-current-continuation )
+(values ) (call-with-values )
+(call-with-input-file ) (call-with-output-file )
